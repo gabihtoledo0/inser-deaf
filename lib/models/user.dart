fb3289ad16ebd256d8487fc39838d30186682cdf
gabihtoledo0 <gabitoledo60@gmail.com>
2020-09-10 20:06:01 -0300

    config db

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,6 +2,24 @@ class User {
   int id;
   String email;
   String senha;
-  String confSenha;
   num phone;
+
+  User(this.id, this.email, this.senha, this.phone);
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic> {
+      'id': id,
+      'email': email,
+      'senha': senha,
+      'phone': phone
+    };
+    return map;
+  }
+
+  User.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    email = map['email'];
+    senha = map['senha'];
+    phone = map['phone'];
+  }
 }
