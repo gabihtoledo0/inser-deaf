3895c82346aa7bd2c928ba7dda15e979ea9d7c81
bolehf <felipebaule@hotmail.com>
2020-11-02 16:05:20 -0300

    Adicionando chamados de intéprete
    adicionado a Model a classe: userCard
    adicionado ao dao a tabela: userCard_dao

--- lib/data/database-helper.dart
+++ lib/data/database-helper.dart
@@ -1,3 +1,4 @@
+import 'package:Inserdeaf/data/dao/userCard_dao.dart';
 import 'package:Inserdeaf/data/dao/user_dao.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -12,6 +13,7 @@ Future<Database> getDatabase() async {
       db.execute(UserDao.tableSql);
       db.execute(InterpreterDao.tableSql);
       db.execute(EstadoDao.tableSql);
+      db.execute(UserCardDao.tableSql);
     },
     version: 1,
   );
