a9bd40096857f7940a226664180b6fb41a9916f1
bolehf <felipebaule@hotmail.com>
2020-11-12 20:17:24 -0300

    Tela de Cadastrar Chamado
    adicionado tela e método de criar chamado

--- lib/models/userCard.dart
+++ lib/models/userCard.dart
@@ -1,24 +1,31 @@
 class UserCard {
   int id;
+  String nome;
+  String telefone;
   String horario;
   DateTime data;
   String cep;
 
-  UserCard(this.id, this.horario, this.data, this.cep);
+  UserCard(
+      this.id, this.nome, this.telefone, this.horario, this.data, this.cep);
 
   @override
   String toString() {
-    return 'UserCard{id: $id, horario: $horario, data: $data, cep: $cep}';
+    return 'UserCard{id: $id, nome: $nome, telefone: $telefone, horario: $horario, data: $data, cep: $cep}';
   }
 
   UserCard.fromJson(Map<String, dynamic> json)
       : id = json['id'],
+        nome = json['nome'],
+        telefone = json['telefone'],
         horario = json['horario'],
         data = json['data'],
         cep = json['cep'];
 
   Map<String, dynamic> toJson() => {
         'id': id,
+        'nome': nome,
+        'telefone': telefone,
         'horario': horario,
         'data': data,
         'cep': cep,
@@ -27,6 +34,8 @@ class UserCard {
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
     map['id'] = id;
+    map['nome'] = nome;
+    map['telefone'] = telefone;
     map['horario'] = horario;
     map['data'] = data;
     map['cep'] = cep;
